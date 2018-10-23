08dde82ed5f15c95a409beb64d7a370b2f8a22ba
David <david.m.host@gmail.com>
2018-10-23 15:29:17 -0400

    Added module docstring

--- Delta-time 1/Game.py
+++ Delta-time 1/Game.py
@@ -1,3 +1,17 @@
+#!/usr/bin/env/python3
+"""Challenge for DAGD 255.
+
+Move a circle accross the screen at 200 px/s. Calculate delta time so that
+movement is not tied to FPS.
+
+Attributes:
+    SIZE (tuple): Define size of the game window in pixels (x, y).
+    BLACK (tuple): Define the color black in RGB values.
+
+Author:
+    CptMikhailov (David Host)
+    Github profile at: https://github.com/CptMikahilov
+"""
 import pygame
 
 
